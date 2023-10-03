b48f11cea632a49407764200aeb181413978c80f
Katia Ghezali <ghezalikatia@hotmail.com>
2023-10-03 10:34:16 +0100

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+
+  const nbar = links.map((link) => (
+  
+        <a key={link} href={"#" + link}>
+          {link}
+        </a>
+    
+  ));
+
+  return <nav> {nbar}</nav>;
 }
 
 export default NavBar;
